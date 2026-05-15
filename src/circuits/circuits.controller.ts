cd25dc94ad7b6cfd1165c9ac979cc75dab3b1cb8
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2026-05-15 11:39:28 -0300

    feat(circuits): adicionar listagem paginada
    Expõe o endpoint GET /circuits no CircuitsController, utilizando PaginationQueryDto para suportar listagem paginada de circuitos.

--- src/circuits/circuits.controller.ts
+++ src/circuits/circuits.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import type { PaginatedResponse } from '../common/interfaces/paginated-response.interface';
 import { CircuitsService } from './circuits.service';
 import { CreateCircuitDto } from './dto/create-circuit.dto';
 import { UpdateCircuitDto } from './dto/update-circuit.dto';
@@ -14,6 +16,11 @@ export class CircuitsController {
   @HttpCode(HttpStatus.CREATED)
   async create(@Body() dto: CreateCircuitDto): Promise<CircuitResponse> {
     return this.circuitsService.create(dto);
+  }
+
+  @Get()
+  async findAll(@Query() query: PaginationQueryDto): Promise<PaginatedResponse<CircuitResponse>> {
+    return this.circuitsService.findAll(query.page ?? 1, query.limit ?? 20);
   }
 
   @Get(':id')
